0e6ccbdba79edfceacf51be21befdae0adc20231
Alex Georgiev Ninov <iamalexgninov@gmail.com>
2024-07-29 21:25:03 +0300

    main route

--- server/app.js
+++ server/app.js
@@ -1,13 +1,19 @@
 require("dotenv").config();
 const express = require("express");
 const db = require("./config/db");
+const routes = require("./routes");
 
 run();
 
 function run() {
   const app = express();
 
+  app.use(express.json());
+  app.use(express.urlencoded({ extends: true }));
+
   db();
+
+  app.use("/api", routes);
 
   app.listen(process.env.PORT, () =>
     console.log(`Server on: ${process.env.PORT}`)
